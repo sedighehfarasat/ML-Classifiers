cac42615eca7c5a5b7c929ffb840b5ac00cd64a0
Sedigheh <sd.farasat@gmail.com>
2022-04-08 22:53:51 +0430

    Added more parameters for classifiers

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ import matplotlib.pyplot as plt
 
 
 def get_dataset(dataset):
-    if dataset == "Iris":
+    """loads dataset from scikitlearn toy datasets"""
+
+    if dataset == "Iris Plants":
         data = datasets.load_iris()
     elif dataset == "Breast Cancer":
         data = datasets.load_breast_cancer()
@@ -20,17 +22,24 @@ def get_dataset(dataset):
 
     x = data.data
     y = data.target
+
     return x, y
 
-
+# TODO: Add more parameters
 def add_model_parameter_ui(classifier):
     params = dict()
     if classifier == "KNN":
         k = st.sidebar.slider("K", 1, 15)
+        weights = st.sidebar.radio("Weights", ["Uniform", "Distance"])
+        algorithm = st.sidebar.radio('Algorithm', ['Auto', 'Ball-tree', 'KD-tree', 'Brute'])
         params["K"] = k
+        params["Weights"] = weights.lower()
+        params["Algorithm"] = algorithm.lower()
     elif classifier == "SVM":
         c = st.sidebar.slider("C", 0.01, 10.0)
+        kernel = st.sidebar.radio('Kernel', ['Linear', 'Poly', 'RBF', 'Sigmoid'])
         params["C"] = c
+        params["Kernel"] = kernel.lower()
     else:
         max_depth = st.sidebar.slider("max_depth", 2, 15)
         n_estimators = st.sidebar.slider("n_estimators", 1, 100)
@@ -41,9 +50,9 @@ def add_model_parameter_ui(classifier):
 
 def get_classifier(classifier, params):
     if classifier == "KNN":
-        clf = KNeighborsClassifier(n_neighbors=params["K"])
+        clf = KNeighborsClassifier(n_neighbors=params["K"], weights=params["Weights"], algorithm=params["Algorithm"])
     elif classifier == "SVM":
-        clf = SVC(C=params["C"])
+        clf = SVC(C=params["C"], kernel=params["Kernel"])
     else:
         clf = RandomForestClassifier(max_depth=params["max_depth"], n_estimators=params["n_estimators"], random_state=1)
     return clf
@@ -55,30 +64,38 @@ if __name__ == '__main__':
     st.write('''
     Play with different datasets and machine learning classifiers to understand data better!
     ''')
+    st.markdown('<hr>', unsafe_allow_html=True)
+
     # Sidebar
-    dataset = st.sidebar.selectbox("Select Dataset", ["Iris", "Breast Cancer", "Wine"])
-    classifier = st.sidebar.selectbox("Select Classifier", ["KNN", "SVM", "Random Forest"])
-    # Section description
+    dataset = st.sidebar.selectbox("Select Dataset", ["Iris Plants", "Breast Cancer", "Wine"])
+    # TODO: Add these classifiers: Decision trees, Adaboost
+    classifier = st.sidebar.selectbox("Select Classifier",
+                                      ["KNN", "SVM", "Random Forest"])
+
+    # Dataset Section
     data_section = st.container()
-    data_section.header("Information about your dataset")
-    # Dataset description
+    data_section.header("Information about the dataset")
     X, y = get_dataset(dataset)
-    data_section.write("Shape of Dataset: ")
-    data_section.write(X.shape)
+    # TODO: Display the list of features
+    data_section.write("Number of Samples: ")
+    data_section.write(len(X))
+    data_section.write("Number of Features: ")
+    data_section.write(np.shape(X)[1])
     data_section.write("Number of Classes: ")
     data_section.write(len(np.unique(y)))
-    # Model description
+    data_section.markdown('<hr>', unsafe_allow_html=True)
+
+    # Model Section
     params = add_model_parameter_ui(classifier)
     clf = get_classifier(classifier, params)
-    # Classification
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
-    data_section.write("Classifier: ")
-    data_section.write(classifier)
-    data_section.write("Accuracy: ")
-    data_section.write(acc)
+    model_section = st.container()
+    model_section.header("Information about the classifier")
+    model_section.write("Accuracy: ")
+    model_section.write(acc)
     # Plotting
     pca = PCA(2)
     X_projected = pca.fit_transform(X)
@@ -89,4 +106,4 @@ if __name__ == '__main__':
     plt.xlabel("Principal Component 1")
     plt.ylabel("Principal Component 2")
     plt.colorbar()
-    data_section.pyplot(fig)
+    model_section.pyplot(fig)
